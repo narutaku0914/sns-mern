f7e8cf15dcb8be1cddd6c08c7affc56fa862d6da
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-18 18:31:31 +0900

    ルーティング設定基礎

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello express");
-});
+// ミドルウェア
+app.use("/api/users", userRoute);
+
+// app.get("/", (req, res) => {
+//   res.send("hello express");
+// });
 
 app.listen(PORT, () => console.log("サーバーが起動しました。"));
