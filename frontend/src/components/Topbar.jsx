c8bdd0a00c804de78ab4e7b95af319714101dba5
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-23 22:47:39 +0900

    Topbarの雛形作成

---
+++ frontend/src/components/Topbar.jsx
@@ -0,0 +1,19 @@
+export const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">MERN</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="serchbar">
+          <input type="text" className="serachInput" placeholder="Search" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="iconItem">1</div>
+        <div className="iconItem">2</div>
+      </div>
+      <img src="" alt="" />
+    </div>
+  );
+};
