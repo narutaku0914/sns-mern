71090fc27e8e3bbcd03ce45020c1482deb4edd1a
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-26 23:01:55 +0900

    leftber、timeline、rightbar作成 Homeのレイアウトも修正

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
+import { Leftbar } from "../../components/leftbar/Leftbar";
+import { Rightbar } from "../../components/rightbar/Rightbar";
+import { Timeline } from "../../components/timeline/Timeline";
 import { Topbar } from "../../components/topbar/Topbar";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <>
       <Topbar />
-      {/* leftbar */}
-      {/* timeline */}
-      {/* rightbar */}
+      <div className="homeContainer">
+        <Leftbar />
+        <Timeline />
+        <Rightbar />
+      </div>
     </>
   );
 };
