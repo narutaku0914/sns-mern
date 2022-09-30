17f00ed296af314260654eaee9aa9a331a04864a
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-30 23:08:52 +0900

    Shareコンポーネント作成、stylingまで完了

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,3 +1,37 @@
+import { Avatar } from "@mantine/core";
+import "./Share.css";
+
 export const Share = () => {
-  return <div>Share</div>;
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <Avatar src="/assets/person/1.jpeg" className="fhareProfileImg" />
+          <input
+            type="text"
+            className="shareInput"
+            placeholder="今なにしてる？"
+          />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareButtons">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <i className="fa-solid fa-image"></i>
+            </div>
+            <div className="shareOption">
+              <i className="fa-solid fa-video"></i>
+            </div>
+            <div className="shareOption">
+              <i className="fa-regular fa-face-grin"></i>
+            </div>
+            <div className="shareOption">
+              <i className="fa-solid fa-chart-column"></i>
+            </div>
+          </div>
+          <button className="shareButton">投稿</button>
+        </div>
+      </div>
+    </div>
+  );
 };
