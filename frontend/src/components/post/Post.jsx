2cb3df8cac908af63e08dcb82e8ae19bc358b06a
narutaku0914 <tkm.nrsw@gmail.com>
2022-10-02 14:10:47 +0900

    投稿、投稿一覧部分作成

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,3 +1,34 @@
+import { Avatar } from "@mantine/core";
+import "./Post.css";
+
 export const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <Avatar src="/assets/person/1.jpeg" className="postProfileImg" />
+            <span className="postUserName">narutaku</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <i className="fa-solid fa-ellipsis-vertical"></i>
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">SNS開発中</span>
+          <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <i className="fa-solid fa-heart"></i>
+            <span className="postLikeCounter">5人のいいね</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentCounter">4: コメント</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
