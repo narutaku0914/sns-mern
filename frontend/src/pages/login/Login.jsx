d3d4cfc54e2c4172f9b80bb26fdbbe670ca77af7
narutaku0914 <tkm.nrsw@gmail.com>
2022-10-06 23:22:59 +0900

    loginページ雛型作成、左側だけスタイリングも

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,15 @@
+import "./Login.css";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">MERN</h3>
+          <span className="logoDescription">Real SNS</span>
+        </div>
+        <div className="loginRight">aaaa</div>
+      </div>
+    </div>
+  );
+};
