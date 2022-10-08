64ace0cbb0ab3fff221502af9170882f466ae1a5
narutaku0914 <tkm.nrsw@gmail.com>
2022-10-08 22:51:23 +0900

    ルーティングの設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-// import { Home } from "./pages/home/Home";
-// import { Profile } from "./pages/profile/Profile";
-// import { Login } from "./pages/login/Login";
+import { Home } from "./pages/home/Home";
+import { Profile } from "./pages/profile/Profile";
+import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <Home /> */}
-      {/* <Profile /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
