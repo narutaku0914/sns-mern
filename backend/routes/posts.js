d97eceb71373397929a478d14c9aeb2207d94de4
narutaku0914 <tkm.nrsw@gmail.com>
2022-10-10 13:07:17 +0900

    フロントからバックに投稿データの取得

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -84,9 +84,9 @@ router.put("/:id/like", async (req, res) => {
 });
 
 // タイムラインの取得
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     // フォローしているユーザーの取得
     const friendPosts = await Promise.all(
